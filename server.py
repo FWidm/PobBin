b70a29ed259673a80a2f43c6117494c5e3f15e45
Teun Beijers <teunbeijers@acknowledge.nl>
2020-10-19 09:13:47 +0200

    move function to a separate file

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ from fastapi.openapi.utils import get_openapi
 
 from pobbin.api.db.database import engine, Base
 from pobbin.api.routes import pastes
+from pobbin.util.paste_xml_request_body_override import paste_xml_request_body_override
 
 Base.metadata.create_all(bind=engine)
 
@@ -25,20 +26,8 @@ def custom_openapi():
         version="1.0.0",
         routes=app.routes,
     )
-    # @TODO: this is very ugly, but it works. It should really be easier to make this work -_-
-    # right now this just replaces the /pastes "post" endpoint documentation and we pretend
-    # it accepts xml
-    openapi_schema["paths"]["/pastes"]["post"]["requestBody"] = {
-        "content": {
-            "application/xml": {
-                "schema": {
-                    "title": "Paste",
-                    "type": "string"
-                }
-            }
-        },
-        "required": True
-    }
+
+    paste_xml_request_body_override(openapi_schema)
 
     app.openapi_schema = openapi_schema
     return app.openapi_schema
